c19323fec63dfab3aedb4f257598931be3b72d2a
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-24 10:33:50 +0000

    done

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 
 <?php
-echo "hello";
 $login = $_GET['login'];
 if (isset($login)) {
 ?>
@@ -11,7 +10,7 @@ if (isset($login)) {
  <title>Php shift</title>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand font" href="#">PW Url</a>
+      <a class="navbar-brand font" href="#">Url Changer</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -51,6 +50,21 @@ if (isset($login)) {
 
 </html>
 <?php } else {
-  echo "<h1>NO LOGIN USER</h1>";
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LOGIN</title>
+ </head>
+ <body>
+  <form method="get" action="index.php">
+    <input type="text" name="login" placeholder="Enter Password To Login" />
+    <input type="submit" name="submit" value="submit" />
+  </form>
+ </body>
+ </html>
+<?php
 }
 ?>
