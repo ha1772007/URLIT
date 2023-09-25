8f67f91f2db1b4920f7e59e96a31eb5889ccedb5
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-25 03:47:44 +0000

    done

--- hide/index.php
+++ hide/index.php
@@ -1,18 +1,23 @@
-<?php 
+<?php
+// Path to the file
 $path = 'https://d26g5bnklkwsh4.cloudfront.net/254c513c-8e1d-4043-9aae-7f35fa7a4ccc/hls/720/main.m3u8';
-$mine_type = mine_content_type($path);
 
+// This is based on file type of $path, but not always needed    
+$mm_type = "application/octet-stream";
+
+//Set headers
 header("Pragma: public");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Cache-Control: public");
 header("Content-Description: File Transfer");
-header("Content-Type: " . $mine_type);
-header("Content-Length: " . (string)(filesize($path)) );
-header('Content-Disposition: attached; filename="' .basename($path).'"');
+header("Content-Type: " . $mm_type);
+header("Content-Length: " .(string)(filesize($path)) );
+header('Content-Disposition: attachment; filename="'.basename($path).'"');
 header("Content-Transfer-Encoding: binary\n");
 
-readfile($path);
+// Outputs the content of the file
+readfile($path); 
 
 exit();
 ?>
