5dc690e475fc5689759182c8a27efb8ba31ffd65
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-25 03:59:11 +0000

    done

--- geturl/index.php
+++ geturl/index.php
@@ -8,5 +8,4 @@ echo "<div id='q'>" . $id . "</div>";
     var id = document.getElementByid('id').innerHTML;
     var q = document.getElementByid('q').innerHTML;
     document.write('<br>' + id + '<br>' + q);
-    window.open('https://d26g5bnklkwsh4.cloudfront.net/' +  +'/hls/720/main.m3u8')
 </script>
