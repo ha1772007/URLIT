b7d1f8a2fdc6d967522edf1d64f88c539ac9d5b9
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-26 04:36:57 +0000

    done

--- geturl/index.php
+++ geturl/index.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GETURL</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
-  $id = $_GET['id'];
-  $q = $_GET['q'];
-  header("Location: https://d26g5bnklkwsh4.cloudfront.net/" . $id ."/hls/" . $q . "/main.m3u8");
+//   $id = $_GET['id'];
+//   $q = $_GET['q'];
+//   header("Location: https://d26g5bnklkwsh4.cloudfront.net/" . $id ."/hls/" . $q . "/main.m3u8");
 ?>
