fe2b31aeb293f0b70ab43011c1970218c105a631
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-27 13:29:13 +0530

    Update index.php

--- geturl/index.php
+++ geturl/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GETURL</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
   $id = $_GET['id'];
   $q = $_GET['q'];
