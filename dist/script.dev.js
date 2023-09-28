ef69f0b11cbc4b22aada577a6a295e39409ed5dc
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-28 03:23:10 +0000

    done

---
+++ dist/script.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function run() {
+  var url = document.getElementById('url').value;
+  var edit = url.split("cloudfront.net")[1];
+  var _final = edit.split("/dash/")[0];
+  var link = "<a  class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + _final + "/hls/720/main.m3u8'>720p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + _final + "/hls/480/main.m3u8'>480p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + _final + "/hls/360/main.m3u8'>360p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + _final + "/hls/240/main.m3u8'>240p</a>" + "<br>"; // document.write(link);
+
+  document.getElementById('link').innerHTML = link;
+}
--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function run() {
     var final = edit.split("/dash/")[0];
     var link = "<a  class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + final + "/hls/720/main.m3u8'>720p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + final + "/hls/480/main.m3u8'>480p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + final + "/hls/360/main.m3u8'>360p</a>" + "<br>" + "<a class='link btn btn-secondary' href='https://d26g5bnklkwsh4.cloudfront.net" + final + "/hls/240/main.m3u8'>240p</a>" + "<br>"
     // document.write(link);
-    document.getElementById('link').innerHTML = link;}
-    
+    document.getElementById('link').innerHTML = link;
+}
