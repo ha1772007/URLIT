f73393b4fd96bf54de0330454f27bd5225a18d0a
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-09-30 02:40:51 +0000

    done

---
+++ indexDown.php
@@ -0,0 +1,28 @@
+<html>
+
+<head></head>
+<body>
+<center>
+<h1>Server Down Due to Some Technical Error</h1>
+<br>
+<spam>
+<br>
+Token:: Expired
+<br>
+Remote-Login:: Not Found;
+<br>
+Trash-Manager:: No Response;
+<br>
+Host-Respose:: True;
+<br>
+Trafic:: Low;
+<br>
+Speed Test Result:: Avg(300mbps);
+<br>
+bandwidth:: full;
+<br>
+</spam>
+<h3>Login to Cpanel as Admin and Check Email For More Details</h3>
+</center>
+</body>
+</html>
