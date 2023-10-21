fe931ba31734d7badc7ff0f7f20aab96e2810ab0
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-10-21 12:22:26 +0000

    done

--- TV/play.php
+++ TV/play.php
@@ -6,7 +6,7 @@
 <body>
     <h1>HTML Video Player</h1>
     <video width="640" height="360" controls>
-        <source src="https://cloud.marvelcloud.tech/download.aspx?file=AWnHr0pocnVPHuq0Z27Nz2jOwQtUtSZGUuitGvmSkKqS8dclUIEZP7uebpeVq615&expiry=PpHPdhkzuv5XI6ghokY13w%3D%3D&mac=da2803f980db071d0d05bfb3672a88fc84e46a18659f58ccf8629baaab03ddf4" type="video/mp4">
+        <source src="url" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 </body>
