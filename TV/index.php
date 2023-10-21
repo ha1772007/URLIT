30d74dd5fd629d2a198bb82f1c4c4afb9d99dce5
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-10-21 11:41:55 +0000

    done

--- TV/index.php
+++ TV/index.php
@@ -1,3 +1,39 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $title = $_POST["title"];
+    $url = $_POST["url"];
+    
+    // Connect to the database (modify these settings)
+    $db_host = 'sql202.infinityfree.com';
+    $db_user = 'if0_35029802';
+    $db_pass = 'DgVdABzbMgk';
+    $db_name = 'if0_35029802_tvshare';
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert data into the database
+    $sql = "INSERT INTO submissions (title, url) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $title, $url);
+
+    if ($stmt->execute()) {
+        echo "Data submitted successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,7 +89,7 @@
 <body>
     <div class="container">
         <h2>Submit Details</h2>
-        <form action="process_form.php" method="post">
+        <form action="index.php" method="post">
             <label for="title">Title:</label>
             <input type="text" id="title" name="title" required>
 
