08b6c2c7f0be803b66ea47c86b7c93a154119922
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-10-21 11:56:23 +0000

    done

--- TV/show.php
+++ TV/show.php
@@ -1,42 +1,71 @@
-<?php
-$db_host = 'sql202.infinityfree.com';
-$db_user = 'if0_35029802';
-$db_pass = 'DgVdABzbMgk';
-$db_name = 'if0_35029802_tvshare';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database Content Table</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin: 20px auto;
+        }
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+        th {
+            background-color: #f2f2f2;
+        }
 
-$sql = "SELECT * FROM submissions";
-$result = $conn->query($sql);
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h2>Database Content Table</h2>
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        // Process the data, e.g., display it in an HTML table
+    <?php
+    // Database connection parameters
+    $db_host = 'sql202.infinityfree.com';
+    $db_user = 'if0_35029802';
+    $db_pass = 'DgVdABzbMgk';
+    $db_name = 'if0_35029802_tvshare';
+
+    // Create a connection to the database
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "No data found.";
-}
 
-if ($result->num_rows > 0) {
-    echo "<table>";
-    echo "<thead><tr><th>ID</th><th>Title</th><th>URL</th></tr></thead>";
-    echo "<tbody>";
+    // SQL query to retrieve data from a table (replace 'your_table' with your table name)
+    $sql = "SELECT * FROM submissions";
+    $result = $conn->query($sql);
 
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["title"] . "</td>";
-        echo "<td><a href='" . $row["url"] . "'>" . $row["url"] . "</a></td>";
-        echo "</tr>";
+    // Display data in an HTML table
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Title</th><th>URL</th></tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["title"] . "</td>";
+            echo "<td>" . $row["url"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "No data found in the table.";
     }
 
-    echo "</tbody>";
-    echo "</table>";
-} else {
-    echo "No data found.";
-}
-?>
+    // Close the database connection
+    $conn->close();
+    ?>
+</body>
+</html>
