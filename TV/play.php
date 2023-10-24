30274a9f988862b89f025de66e1ce83ec1f76eee
ha1772007 <72550454+ha1772007@users.noreply.github.com>
2023-10-24 07:32:51 +0000

    done

--- TV/play.php
+++ TV/play.php
@@ -2,7 +2,7 @@
 
 <?php
 if(isset($_GET['url'])){
-    $url = $_POST['url'];
+    $url = $_GET['url'];
 
     ?>
 
@@ -13,12 +13,13 @@ if(isset($_GET['url'])){
 </head>
 <body>
 
-<video id="myVideo" controls autoplay muted playsinline>
-    <source src="your-video.mp4" type="video/mp4">
+<video id="myVideo" width="100%" height="100%" controls autoplay muted playsinline>
+    <source src="<?php echo $url; ?>" type="video/mp4">
     Your browser does not support the video tag.
 </video>
 
 <script>
+    document.addEventListener('DOMContentLoaded', function() {
     // Auto fullscreen (Note: Not supported in all browsers)
     const video = document.getElementById('myVideo');
     video.requestFullscreen = video.requestFullscreen || video.mozRequestFullScreen || video.webkitRequestFullscreen || video.msRequestFullscreen;
@@ -26,6 +27,26 @@ if(isset($_GET['url'])){
 
     // Auto unmute (Auto audio on by default)
     video.muted = false;
+});
+
+
+    video.addEventListener('click', function() {
+        // Check if the video is paused
+        if (video.paused) {
+            // Play the video
+            video.play();
+            
+            // Auto fullscreen (Note: Not supported in all browsers)
+            const requestFullScreen = video.requestFullscreen || video.mozRequestFullScreen || video.webkitRequestFullscreen || video.msRequestFullscreen;
+            if (requestFullScreen) {
+                requestFullScreen.call(video);
+            }
+        } else {
+            // If the video is already playing, clicking it will pause it
+            video.pause();
+        }
+    });
+
 </script>
 
 </body>
@@ -42,7 +63,7 @@ if(isset($_GET['url'])){
     </head>
     <body>
         <h1>Enter url To play</h1>
-        <footer>Disclaimer: we Does not Host Any Content On Our Own We only Play Content From Url Provided By The User</footer>
+        <footer>Disclaimer: We Does Not Host Any Content On Our Own We Only Play Content From Url Provided By The User</footer>
     </body>
     </html>
     <?php
